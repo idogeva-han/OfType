e6611b4ebefa2481e3d95735570d786db9ca6550
Ido Geva <ido.geva.b@gmail.com>
2025-06-22 19:09:37 +0300

    implement GetOfType

---
+++ OfType/TypeChecker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfType
+{
+    internal class TypeChecker : IOfType
+    {
+        public IEnumerable<T> GetOfType<T>(IEnumerable enumerable)
+        {
+            return enumerable.OfType<T>();
+        }
+
+        public IEnumerable<TOutput> GetOfType<TSource, TOutput>(IEnumerable<TSource> enumerable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TBase> OfBase<TBase, TDerived>(IEnumerable<TDerived> derivedItems) where TDerived : TBase
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
